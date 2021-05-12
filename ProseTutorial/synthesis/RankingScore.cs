209ed8c958c46ab2b0dcccb850f71bf1de8678a7
DhvanilSanghvi <sanghvidhvanil@gmail.com>
2021-05-12 13:24:59 +0530

    Updated Ranking

--- ProseTutorial/synthesis/RankingScore.cs
+++ ProseTutorial/synthesis/RankingScore.cs
@@ -15,37 +15,57 @@ namespace ProseTutorial
             return 0;
         }
 
-        [FeatureCalculator(nameof(Semantics.Add))]
-        public static double Add(double v, double start, double end)
+        // static doesnt work because, we cannot reference it by "this"
+        // Documentation not in Ranking but in "Features"
+        
+        [FeatureCalculator(nameof(Semantics.Add), Method = CalculationMethod.FromChildrenNodes)]
+        public double Add(VariableNode v, ProgramNode start, ProgramNode end) 
         {
-            if(start + end == 0)    return 1;
-            return 1/(start + end);
+            double score = (double) start.GetFeatureValue(this) + (double) end.GetFeatureValue(this);
+            if(score == 0)    return 1;
+            return 1/score;
         }
 
         
 
-        [FeatureCalculator(nameof(Semantics.Mul))]
-        public static double Mul(double v, double start, double end)
+        // [FeatureCalculator(nameof(Semantics.Mul))]
+        // public  double Mul(double v, double start, double end)
+        // {
+        //     if(start * end == 0)    return 1;
+        //     return 1/(start * end);
+        // }
+
+        [FeatureCalculator(nameof(Semantics.Mul), Method = CalculationMethod.FromChildrenNodes)]
+        public double Mul(VariableNode v, ProgramNode start, ProgramNode end) 
         {
-            if(start * end == 0)    return 1;
-            return 1/(start * end);
+            double score = (double) start.GetFeatureValue(this) * (double) end.GetFeatureValue(this);
+            if(score == 0)    return 1;
+            return 1/score;
         }
 
-        [FeatureCalculator(nameof(Semantics.Div))]
-        public static double Div(double v, double start, double end)
+        // [FeatureCalculator(nameof(Semantics.Div))]
+        // public  double Div(double v, double start, double end)
+        // {
+        //     if(start * end == 0)    return 1; 
+        //     return 1/(start * end);
+        // }
+
+        [FeatureCalculator(nameof(Semantics.Div), Method = CalculationMethod.FromChildrenNodes)]
+        public double Div(VariableNode v, ProgramNode start, ProgramNode end) 
         {
-            if(start * end == 0)    return 1; 
-            return 1/(start * end);
+            double score = (double) start.GetFeatureValue(this) * (double) end.GetFeatureValue(this);
+            if(score == 0)    return 1;
+            return 1/score;
         }
 
         [FeatureCalculator(nameof(Semantics.Element))]
-        public static double Element(double v, double k)
+        public double Element(double v, double k)
         {
             return 1;
         }
 
         [FeatureCalculator("k", Method = CalculationMethod.FromLiteral)]
-        public static double K(int k)
+        public double K(int k)
         {
             if(k == 0){
                 return 1;
